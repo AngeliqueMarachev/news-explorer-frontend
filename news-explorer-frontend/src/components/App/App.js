dbe777492295eb44f90e23a4666a500a040a8a09
AngeliqueMarachev <angeliquealive@gmail.com>
2023-01-11 11:28:50 +0200

    Implement Routes

--- news-explorer-frontend/src/components/App/App.js
+++ news-explorer-frontend/src/components/App/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Main from "../Main/Main";
+import SavedNews from "../SavedNews/SavedNews";
 
 export default function App() {
   return (
-  <p>App</p>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/saved-news" element={<SavedNews />} />
+    </Routes>
   );
 }
