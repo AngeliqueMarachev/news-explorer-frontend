46ece564f740cb0dcd2a02f9025669b140bc5893
AngeliqueMarachev <angeliquealive@gmail.com>
2023-01-17 12:34:50 +0200

    Feat: Footer component

--- news-explorer-frontend/src/components/App/App.js
+++ news-explorer-frontend/src/components/App/App.js
@@ -3,18 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
-// import Navigation from "../Navigation/Navigation";
 import Hero from "../Hero/Hero";
+import Footer from "../Footer/Footer";
 
 export default function App() {
   return (
     <section className="content">
-      {/* <Navigation /> */}
       <Hero />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/saved-news" element={<SavedNews />} />
       </Routes>
+      <Footer />
     </section>
   );
 }
