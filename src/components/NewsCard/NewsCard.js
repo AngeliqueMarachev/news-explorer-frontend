20816629f95c5f3bc57ba09335fd729b7a535dea
AngeliqueMarachev <angeliquealive@gmail.com>
2023-02-01 09:31:17 +0200

    Add loction to News Card

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -8,6 +8,7 @@ export default function NewsCard({
   title,
   text,
   source,
+  keyword,
   isSaved,
 }) {
   const location = useLocation();
@@ -17,11 +18,26 @@ export default function NewsCard({
       <img className="news-card__image" src={image} alt={title} />
 
   
-      
-      <button className="news-card__save" />
+      {location.pathname === "/" ? (
+       <button className="news-card__save" />
+      ) : (
+        <button className="news-card__delete" />
+      )}
+     
       <div className="news-card__label">
-        <p className="news-card__signin-text">Sign in to save articles</p>
+        <p className="news-card__signin-text">
+          {location.pathname === "/" ? (
+          "Sign in to save articles"
+          ) : (
+            "Remove from saved"
+          )}
+          </p>
       </div>
+      {location.pathname === '/saved-news' && (
+        <div className="news-card__label news-card__label_keyword">
+          <p className="news-card__label news-card__label_text">{keyword}</p>
+          </div>
+      )}
       <div className="news-card__main">
         <p className="news-card__date">{date}</p>
         <h3 className="news-card__title">{title}</h3>
@@ -31,41 +47,3 @@ export default function NewsCard({
     </div>
   );
 }
-  
-// export default function NewsCard({ image, date, title, text, source, keyword }) {
-//   const location = useLocation();
-
-//   return (
-//     <div className="news-card">
-//       <img className="news-card__image " src={image} alt={title} />
-//       {location.pathname === "/" ? (
-//         <button className="news-card__button news-card__button_save" />
-//       ) : (
-//         <button className="news-card__button news-card__button_delete" />
-//       )}
-
-//       <div className="news-card__label news-card__label_sign-in">
-//         <p className="news-card__signin-text">
-//           {location.pathname === "/" ? (
-//             "Sign in to save articles"
-//           ) : (
-//             "Remove from saved"
-//           )}
-//         </p>
-//       </div>
-
-//       {location.pathname === '/saved-news' && (
-//         <div className="news-card__label news-card__label_keyword">
-//           <p className="news-card__label-text">{keyword}</p>
-//           </div>
-//       )}
-
-//       <div className="news-card__main">
-//         <p className="news-card__date">{date}</p>
-//         <h3 className="news-card__title">{title}</h3>
-//         <p className="news-card__text">{text}</p>
-//         <p className="news-card__source">{source}</p>
-//       </div>
-//     </div>
-//   );
-// }
