da546a5d1979545a29df865ca663f54317dd4337
AngeliqueMarachev <angeliquealive@gmail.com>
2023-02-21 16:32:34 +0200

    Fix search form layout

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -102,55 +102,11 @@ export default function MobileMenu({
                   onClick={handleBurgerMenuClick}
                 ></button>
               </div>
-            </div>
+              </div>
+
           </nav>
         )}
       </nav>
     </>
   );
 }
-//           <div className="mobile-menu__container">
-//          <div className="mobile-menu__header">
-//           <Link to='/' className={`mobile-menu__logo ${navClass && "mobile-menu__logo_saved"}`}>NewsExplorer</Link>
-//            <button type="button" className="mobile-menu__close" onClick={handleBurgerCloseClick}></button>
-//          </div>
-
-//           <div className="mobile-menu__links">
-//             <NavLink to="/" className="mobile-menu__link">
-//               Home
-//             </NavLink>
-
-//             {isLoggedIn ? (
-//               <>
-// <NavLink to="/saved-news" className="mobile-menu__link">
-//   Saved articles
-// </NavLink>
-//                 <button className={`mobile-menu__button mobile-menu__button_logout`}
-//                  username={username}>Elise
-//                   <img src={logout_white} alt="logout logo" />
-//                 </button>
-//               </>
-//             ) : (
-// <button
-//   className={`mobile-menu__button mobile-menu__button_login`}
-//   type="button"
-//   // onClick={() => setIsLoginPopupOpen(true)}
-//   onClick={onSigninClick}
-// >
-//   Sign in
-// </button>
-//             )}
-//           </div>
-//         </div>
-//       ) : (
-//         <>
-//           <NavLink to="/" className={`mobile-menu__logo mobile-menu__logo` + navClass}>
-//             NewsExplorer
-//           </NavLink>
-//           <button
-//             className={`mobile-menu__burger mobile-menu__burger` + navClass}
-//             onClick={handleBurgerMenuClick}
-//           />
-//         </>
-//       )} */
-// }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,23 @@
 import React from "react";
-import './SearchForm.css';
+import "./SearchForm.css";
 
 export default function SearchForm() {
-    return (
-        <div className="search-form">
-            <div className="search-form__container"> 
-                <input className="search-form__input" type="text" placeholder="Enter Topic"/>
-                <button className="search-form__submit" type="button">Search</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="search-form">
+      <div className="search-form__container">
+        <form className="search-form__container-form">
+          <label className="search-form__container-form-field">
+            <input
+              className="search-form__input"
+              type="text"
+              placeholder="Enter Topic"
+            />
+            <button className="search-form__submit" type="submit">
+              Search
+            </button>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 }
