0424c8534b8535334ba597cfbf1bbb3497cb541f
AngeliqueMarachev <angeliquealive@gmail.com>
2023-03-17 15:21:25 +0200

    Feat: Load more articles onClick

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewsCardList.css";
 import NewsCard from "../NewsCard/NewsCard";
 // import { news } from '../../utils/temp_articles.js';
 
-export default function NewsCardList({ articles, isLoggedIn, onSaveClick, savedArticles }) {
+export default function NewsCardList({ articles, isLoggedIn }) {
+  const [showMore, setShowMore] = useState(3);
+
+  const handleShowMoreClick = () => {
+    setShowMore((prevShowMore) => prevShowMore + 3);
+  };
+
   return (
     <>
       <section className="news-list">
         <h4 className="news-list__title">Search results</h4>
-       
-        <ul className="news-list__articles">
-          {articles && articles.slice(0,3).map((article, index) => {
-            console.log(article)
-            return (
-              <li key={index}>
-              <NewsCard
-                image={article.image}
-                date={article.date}
-                title={article.title}
-                text={article.text}
-                source={article.source}
-                isLoggedIn={isLoggedIn}
-                />
-                </li>
-            );
-          })}
-        </ul>
-        
-      
-      </section>
-      <div className="new-list__more">
-        <button className="news-list__more-button" type="button">
-          Show more
-        </button>
-      </div>
-    </>
 
+        <ul className="news-list__articles">
+          {articles &&
+            articles.slice(0, showMore).map((article, index) => {
+              console.log(article);
+              return (
+                <li key={index}>
+                  <NewsCard
+                    image={article.image}
+                    date={article.date}
+                    title={article.title}
+                    text={article.text}
+                    source={article.source}
+                    isLoggedIn={isLoggedIn}
+                  />
+                </li>
+              );
+            })}
+        </ul>
+      </section>
+    
+      <div className="new-list__more">
+      {articles.length && (
+          <button className="news-list__more-button" type="button" onClick={handleShowMoreClick}>
+            Show more
+          </button>
+      
+        )}
+          </div>
+    </>
   );
 }
-
-
